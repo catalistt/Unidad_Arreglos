74e4c20af5704f58ec99369eac6f5d4732753b2e
Catalina <caliste@fen.uchile.cl>
2020-06-01 23:15:26 -0400

    first commit

---
+++ scanaddicts2.rb
@@ -0,0 +1,20 @@
+def scan_addicts2(array)
+    results = []
+    n = array.count
+
+    n.times do |i|
+        if array[i] < 90
+        results.push 'Bien'
+        elsif array[i] >= 90 && array[i] <180
+            results.push 'Mejorable'
+        else
+        results.push 'Mal'
+        end
+    end
+    putsresults
+end
+
+#Ejemplo
+a = [89,90,180]
+scan_addicts2(a)
+
